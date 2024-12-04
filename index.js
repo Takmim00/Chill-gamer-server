b683961b5c48802995f1c3767d899b7e1888657c
Takmim00 <takmimm@gmail.com>
2024-12-04 17:55:42 +0600

    review get and post added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxfxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,15 +25,32 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    
+    const reviewCollection = client.db('reviewDB').collection('review')
+
+    app.get('/review', async (req,res) => {
+        const cursor = reviewCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post('/review', async (req,res) => {
+        const addReview = req.body;
+        console.log(addReview);
+        const result = await reviewCollection.insertOne(addReview)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
